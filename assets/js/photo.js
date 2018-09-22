6dd7209eafca3b44af9759a9debed2cf69f88622
Roger <rgbyford@gmail.com>
2018-09-21 22:18:40 -0500

    reasonable working model

--- assets/js/photo.js
+++ assets/js/photo.js
@@ -3,41 +3,119 @@ $(document).ready(function () {
     const apiUrl = 'http://api.giphy.com/v1/gifs/search';
 
     const queryParams = {
+        q: "",
         api_key: apiKey,
         limit: 10,
         type: 'gif',
-        //    rating: 'g',
-        q: "kangaroo"
+        rating: 'pg'
     };
 
-    let jsonString = JSON.stringify(queryParams);
-    jsonString = jsonString.replace(/{|}/g, ''); // get rid of braces
-    jsonString = jsonString.replace(/"/g, ''); // and quotes
-    jsonString = jsonString.replace(/,/g, "&"); // replace , with &
-    jsonString = jsonString.replace(/:/g, "="); // replace : with =
-    let url = apiUrl + '?' + jsonString;
+    var namesArray = ["cat", "dog", "mouse", "budgerigar", "llama", "canary", "tiger", "elephant", "whale"];
+    var moving = [];
 
-    //var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=PKC8Rfl2DIQL56w5cRipJlX30iISxJBv&limit=5");
-    var xhr = $.get(url);
-    xhr.done(function (pictures) {
-        console.log("success got data", pictures);
-        console.log(`Data: ${pictures.data.length}`);
-        for (var i = 0; i < pictures.data.length; i++) {
-            console.log("number " + i);
-            console.log(pictures.data[i]);
-            console.log(`HTML string: src='${pictures.data[i].url}'`);
-            //        $(".pictures").append (`<img src='${pictures.data[i].url}' alt='help' style='width: 100px; height: 100px'>`);
-            //$(".pictures").append (`<iframe src="https://giphy.com/embed/5FM4518vPXoru" width="480" height="219" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/kangaroo-5FM4518vPXoru"></a></p>`);
-            //$(".pictures").append (`<iframe id="pict" src="${pictures.data[i].embed_url}" width="480" height="219" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/kangaroo-5FM4518vPXoru"></a></p>`);
-            //      $(".pictures").append(`<iframe id="pict" src="${pictures.data[i].embed_url}" width="480" height="219" frameBorder="0" class="giphy-embed"></iframe><p><a href="${pictures.data[i].embed_url}"></a></p>`);
-            //        $(".pictures").append(`<iframe id="pict" src="${pictures.data[i].images['480w_still']}" width="480" height="219" frameBorder="0" class="giphy-embed"></iframe>`);
-            //        $(".pictures").append(`<img class="pict" src="${pictures.data[i].embed_url}" width="480" height="219" frameBorder="0" class="giphy-embed">`);
-            $(".pictures").append('<img class="pict" src="./assets/unhappy-Thomas.jpg" width="480" height="219">');
-            $(".pictures").append(`<img class="pict" src="https://giphy.com/embed/5FM4518vPXoru" width="480" height="219" frameBorder="0">`);
-            $(".pict").stop();
-            //        $.fx.off = true;
-            //        $("#pict").freezeframe();
-        }
-        //    https://giphy.com/gifs/5FM4518vPXoru/html5
+    buildButtonArray();
+
+    function buildButtonArray() {
+        $.each(namesArray, function (index) {
+            moving.push(false);
+            addButton(index);
+        });
+    }
+
+    function addButton(index) {
+        var r = $('<input/>').attr({
+            type: "button",
+            //            id: namesArray[index],
+            value: namesArray[index],
+            //            style: "height: 40px",
+            class: "button_css",
+        });
+        $(".buttons").append(r);
+    }
+
+    $(document).on("click", ".buttons", function () {
+        buildPictures($(document.activeElement).val(), false);
     });
+
+    var newOne;
+    
+    $(document).on("click", "#addAnimalButton", function () {
+        newOne = $('#addAnimal').val();
+        if ($.inArray(newOne, namesArray) != -1) {
+            $("#addOneMsg").text('Already got ' + newOne + '!');
+        } else {
+            var qp = {
+                sp: 'animal*',
+                ml: newOne,
+            }
+            var urlToUse = 'http://api.datamuse.com/words/?' + $.param(qp);
+            var xhr = $.get(urlToUse);
+            xhr.done(function (animals) {
+                if (animals.length == 0) {
+                    $("#addOneMsg").text(newOne + ' is not an animal!');
+                } else {
+                    buildPictures(newOne, true);
+                }
+            });
+        }
+        $("#addAnimal").val('');
+    });
+
+    var picturesKeep;
+
+    function buildPictures(animalName, testing) { // testing is to see if entered animal name is real
+        queryParams.q = animalName;
+        let url = apiUrl + '?' + $.param(queryParams);
+        if (!testing) {
+            $(".pictures").empty();
+        }
+        var xhr = $.get(url);
+        xhr.done(function (pictures) {
+            if (!testing) {
+                picturesKeep = pictures;
+                for (var i = 0; i < pictures.data.length; i++) {
+                    $(`#caption${i}`).text (`Rating: ${pictures.data[i].rating}`);
+                    $(`#pict${i}`).attr ('src', `${pictures.data[i].images.fixed_height_small_still.url}`);                    
+                    // $(".col").append (`<p class="caption">Rating: ${pictures.data[i].rating}</p>`);
+                    // $(".col").append(
+                    //     `<img class="img-responsive" id="pict${i}" src="${pictures.data[i].images.fixed_height_small_still.url}" frameBorder="0">`);
+                }
+            } else {
+                if (pictures.data.length > 0) {
+                    namesArray.push(newOne);
+                    moving.push(false);
+                    addButton(namesArray.length - 1);
+                } else {
+                    $("#addOneMsg").text('No pictures of ' + newOne + '. :-(');
+                }
+            return (pictures.data.length);
+            }
+        });
+    }
+
+
+    // picture click
+    $(document).on("click", ".img-responsive", pictureClick);
+
+    function pictureClick() {
+        let index;
+
+        // get the picture number
+        name = $(this).attr('id');
+        name = name.replace('pict', '');
+        index = parseInt(name);
+
+        // look at the moving array 
+        let movingSrc = `${picturesKeep.data[index].images.fixed_height_small.url}`;
+        let stillSrc = `${picturesKeep.data[index].images.fixed_height_small_still.url}`;
+        // use attr to set the src of the img on this
+        if (moving[index] == true) {
+            $(this).attr('src', stillSrc);
+            moving[index] = false;
+        } else {
+            $(this).attr('src', movingSrc);
+            moving[index] = true;
+            name = namesArray[index];
+        }
+    }
 });
